5835e4cc610be2139553082529f65e240135cd49
ranjantongbram <ranjantongbram@gmail.com>
2024-11-24 21:57:00 +0530

    demooo

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,10 +5,10 @@ const Dashboard = () => {
   return (
     <div style={{display:'flex', flexDirection:'row'}}>
        <div style={{width:'20%',backgroundColor:'royalblue',height:'100vh'}}>
-        <Link to='/addStudent' style={{color:'white',display:'block'}}>Add Student</Link>
-        <Link to='/studentList' style={{color:'white',display:'block'}}>Student List</Link>
-        <Link to='/addFaculty' style={{color:'white',display:'block'}}>Add Faculty</Link>
-        <Link to='/facultyList' style={{color:'white',display:'block'}}>Faculty List</Link>
+        <Link to='/dashboard/addStudent' style={{color:'white',display:'block'}}>Add Student</Link>
+        <Link to='/dashboard/studentList' style={{color:'white',display:'block'}}>Student List</Link>
+        <Link to='/dashboard/addFaculty' style={{color:'white',display:'block'}}>Add Faculty</Link>
+        <Link to='/dashboard/facultyList' style={{color:'white',display:'block'}}>Faculty List</Link>
        </div>
        <div style={{width:'80%',backgroundColor:'wheat',height:'100vh'}}>
         <Outlet/>
